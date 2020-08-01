5e0237b0002aa7432a4bb3619073fc886ef658e2
Langqi Yan <yvonneyan@Narath.local>
2020-07-31 23:27:23 -0700

    server can recieve single message from a client

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+
+UDP_IP = '127.0.0.1'
+UDP_PORT = 8080
+BUFFER = 1024
+MSG = 'hihi'
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MSG)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(MSG.encode('utf-8'), (UDP_IP, UDP_PORT))
