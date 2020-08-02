6d0e0dfd280607d95b72790dfafe916f0ea347c6
Langqi Yan <yvonneyan@Narath.local>
2020-08-02 14:41:44 -0700

    server now can respond when receiving a message

--- client.py
+++ client.py
@@ -10,4 +10,11 @@ print("UDP target port: %s" % UDP_PORT)
 print("message: %s" % MSG)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 sock.sendto(MSG.encode('utf-8'), (UDP_IP, UDP_PORT))
+
+
+data, server = sock.recvfrom(BUFFER)
+print(data.decode('utf-8'))
+sock.close()
+
--- server.py
+++ server.py
@@ -3,13 +3,19 @@ import socket
 UDP_IP = '127.0.0.1'
 UDP_PORT = 8080
 BUFFER = 1024
+RESP = 'hello from the server!'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+server_addr = (UDP_IP, UDP_PORT)
+sock.bind(server_addr)
 
 while True:
 	data, addr = sock.recvfrom(BUFFER)
 	print(addr)
 	print(data.decode('utf-8'))
-	# print("receive from: " + addr)
-	# print("received message: " + data.decode('utf-8'))
+
+	if data:
+		sent = sock.sendto(RESP.encode('utf-8'), addr)
+		print("response sent!")
+		# sock.close()
+	
