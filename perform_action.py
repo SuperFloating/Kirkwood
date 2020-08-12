ae6d4b46eaf055f73ce0621e1aad7a1ec8619bed
Langqi Yan <yvonneyan@Narath.local>
2020-08-12 14:18:02 -0700

    perform_action.py now can connect to kirkwood db on aws

--- perform_action.py
+++ perform_action.py
@@ -1,3 +1,15 @@
+import pandas as pd
+import pymysql
+
+host = "kirkwooddb.chvh78zduuko.us-west-2.rds.amazonaws.com"
+port = 3306
+dbname = "test"
+user = "admin"
+password = "KirkwoodDB"
+
+conn = pymysql.connect(host, user = user, port = port,
+                           passwd = password, db = dbname)
+
 def send_msg():
 	print("sending message")
 
