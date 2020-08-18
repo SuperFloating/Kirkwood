370d8c5a76f292263b3582f95bb3643f07bb5e1c
Langqi Yan <yvonneyan@Narath.local>
2020-08-18 10:13:07 -0700

    client.py will quit if not receiving a response from the server within a 5 sec timeout period

--- client.py
+++ client.py
@@ -1,21 +1,18 @@
 import socket
 import json
+# import socket.timeout as TimeoutException
 
 # local host
 # UDP_IP = '127.0.0.1'
 # aws host
-
 with open('credential.json') as c:
 	credential = json.load(c)
-
 UDP_IP = credential['ip']
 UDP_PORT = credential['port']
 BUFFER = 4096
-
 MSG = ""
 with open('template.json') as f:
 	data_in_json = json.load(f)
-
 # print(data_in_json)
 MSG = json.dumps(data_in_json)
 
@@ -24,10 +21,15 @@ print("UDP target port: %s" % UDP_PORT)
 print("message: %s" % MSG)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+sock.settimeout(5)
 sock.sendto(MSG.encode('utf-8'), (UDP_IP, UDP_PORT))
 
-data, server = sock.recvfrom(BUFFER)
-print(data.decode('utf-8'))
+data = ''
+try:
+	data, server = sock.recvfrom(BUFFER)
+except socket.timeout:
+	print("Timeout!")
+if data != '':
+	print(data.decode('utf-8'))
 sock.close()
 
