3595f7290816203b0b8b05dad11949ebb570f33c
Langqi Yan <yvonneyan@Narath.local>
2020-08-21 13:47:06 -0700

    messages now can get a unique id when being stored into db

--- parse_request.py
+++ parse_request.py
@@ -3,6 +3,6 @@ import perform_action as pa
 
 def parse_request(request):
 	if(request["action"] == "send"):
-		pa.send_msg(request)
+		pa.store_msg(request)
 	if(request["action"] == "retrieve"):
 		pa.retrieve_msg(request)
--- perform_action.py
+++ perform_action.py
@@ -12,23 +12,39 @@ password = credential['db_password']
 
 
 conn = pymysql.connect(host, user = user, port = port,
-                           passwd = password, db = schema)
+						   passwd = password, db = schema)
 
 # reads in a json object request
-def send_msg(request):
-	print("sending message")
+def store_msg(request):
+	print("storing message...")
+
+	# gets an id for this message
+	try:
+		store_msg.msgid += 1
+	except AttributeError:
+		with conn.cursor() as cur: 
+			cur.execute("SELECT MAX(messageid) FROM message")
+			fetchid = cur.fetchone()
+		print(fetchid)
+		if fetchid[0] == None: 
+			store_msg.msgid = 0
+		else: 
+			store_msg.msgid = fetchid[0] + 1
+
+
 	target = request['message']['chatroomid']
 	author = request['message']['userid']
 	time = request['message']['clock']
 	content = request['message']['content']
-	msg_info = "(%d, %d, \"%s\", \"%s\", \"%s\");" % (1, target, author, time, content)
+	msg_info = "(%d, %d, \"%s\", \"%s\", \"%s\");" % (store_msg.msgid, target, author, time, content)
 	with conn.cursor() as cur:
 		print("cp1")
 		cur.execute("INSERT INTO message (messageid, chatroomid, userid, clock, content) VALUES " + msg_info)
 	conn.commit()
 	print("cp2")
+	
 
 
 # reads in a json object request
 def retrieve_msg(request):
-	print("retrieving message")
+	print("retrieving message...")
