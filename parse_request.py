3be8b2f5a4438ebbb3d919aaa44520f96bdd05f0
Hannah Yan <langqi.yan@gmail.com>
2020-08-24 19:39:46 -0700

    wording

--- parse_request.py
+++ parse_request.py
@@ -2,7 +2,7 @@ import json
 import perform_action as pa
 
 def parse_request(request):
-	if(request["action"] == "send"):
+	if(request["action"] == "store"):
 		pa.store_msg(request)
 	if(request["action"] == "retrieve"):
 		pa.retrieve_msg(request)
